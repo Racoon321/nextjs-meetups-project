14823952b8ed78d195520c7630254ef362ad853b
Racoon321 <108225637+Racoon321@users.noreply.github.com>
2022-08-06 23:38:14 +0530

    Update new-meetup.js

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -5,7 +5,7 @@ async function handler(req, res) {
     const data = req.body;
 
     const client = await MongoClient.connect(
-      "..."
+      "mongodb+srv://{process.env.DB_USER}:{process.env.DB_PASSWORD}@cluster0.xxjllg5.mongodb.net/{process.env.DB_NAME}?retryWrites=true&w=majority"
     );
 
     const db = client.db();
