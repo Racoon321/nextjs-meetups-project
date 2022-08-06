1518ec5266ba5059d1acad44e2bdaf2c7ffb3224
Racoon321 <108225637+Racoon321@users.noreply.github.com>
2022-08-06 23:39:21 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ function HomePage(props) {
 
 export async function getStaticProps() {
   const client = await MongoClient.connect(
-    "mongodb+srv://VaibhavDubey:Matpet1234!@cluster0.xxjllg5.mongodb.net/meetups?retryWrites=true&w=majority"
+    "mongodb+srv://{process.env.DB_USER}:{process.env.DB_PASSWORD}@cluster0.xxjllg5.mongodb.net/{process.env.DB_NAME}?retryWrites=true&w=majority"
   );
   const db = client.db();
   const meetupsCollection = db.collection("meetups");
